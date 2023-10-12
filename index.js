91fe342d023a2645ac4d55dcb608b61b3ff0a4ca
josephmangara <josephmangara2@gmail.com>
2023-10-12 11:08:54 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+
+const terms = docment.body.createElement('h1');
+hi.innerText='victory';
+
+document.getElementById("h1")[0].setAttribute("id","victory");
+
+const main = document.getElementById("main");
+while (main.firstChild){
+    main.removeChild(element.firstChild);
+}
+
